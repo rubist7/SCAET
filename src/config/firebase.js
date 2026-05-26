ad0d9733e4eba0bc80cfe62d25c4dc6bf13f7d63
rubist7 <rubisarao2006@gmail.com>
2026-05-26 15:15:22 -0500

    feat: implementar variables de entorno para proteger las credenciales

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBVeQRNTuRyLPVWKUu0x3y56cib71aqZ7M",
-  authDomain: "scaet-f8d1f.firebaseapp.com",
-  projectId: "scaet-f8d1f",
-  storageBucket: "scaet-f8d1f.firebasestorage.app",
-  messagingSenderId: "916795230217",
-  appId: "1:916795230217:web:2dd1870e8064a992f910d8"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Inicializamos Firebase en nuestro Front-End
